74fce3156e12e0fed9f3f12668cefa6eec9a574c
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2025-07-05 14:50:11 -0300

    Adicionando as classes de security

---
+++ src/main/java/com/targetcar/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,32 @@
+package com.targetcar.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private String numero;
+    @Column(name = "complemento", length = 100)
+    private String complemento;
+    @Column(name = "cidade", length = 150)
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+    @Column(name = "cep", length = 9)
+    private String cep;
+}
